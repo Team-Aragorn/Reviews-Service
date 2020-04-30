8910bd7a1a255d0cff063144aaf74f21ccf41bd3
Adam Tiller <a.j.tiller@gmail.com>
2020-04-30 16:08:58 -0700

    Add API route for GETting review data.

--- server/db/index.js
+++ server/db/index.js
@@ -1,50 +1,40 @@
 const mongoose = require('mongoose');
-const seed = require('./seeder.js');
 
 mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
+const reviewSchema = new mongoose.Schema({
+  gameId: Number,
+  date: { type: Date, default: Date.now },
+  overall: Number,
+  title: String,
+  review: String,
+  recommend: Boolean,
+  nickname: String,
+  location: String,
+  email: String,
+  buyForSelf: Boolean,
+  ageBracket: Number,
+  gender: Number,
+  graphics: Number,
+  gameplay: Number,
+  appeal: Number,
+  ownershipBracket: Number,
+  purchaseOnline: Boolean,
+  readReviews: Boolean,
+  recommendBGS: Number,
+  meta: {
+    helpful: Number,
+    unhelpful: Number,
+  },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log('Connection to database successful!');
-  const reviewSchema = new mongoose.Schema({
-    gameId: Number,
-    date: { type: Date, default: Date.now },
-    overall: Number,
-    title: String,
-    review: String,
-    recommend: Boolean,
-    nickname: String,
-    location: String,
-    email: String,
-    buyForSelf: Boolean,
-    ageBracket: Number,
-    gender: Number,
-    graphics: Number,
-    gameplay: Number,
-    appeal: Number,
-    ownershipBracket: Number,
-    purchaseOnline: Boolean,
-    readReviews: Boolean,
-    recommendBGS: Number,
-    meta: {
-      helpful: Number,
-      unhelpful: Number,
-    },
-  });
-
-  const Review = mongoose.model('Review', reviewSchema);
-
-  // UNCOMMENT BELOW TO DROP COLLECTION AND RESEED DATA
-  //
-  // mongoose.connection.db.dropCollection('reviews', (err) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return;
-  //   }
-  //   seed.seed(Review, 100);
-  // });
 });
 
-module.exports.db = db;
+module.exports.Review = Review;
