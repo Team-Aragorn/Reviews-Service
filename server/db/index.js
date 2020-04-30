2031022aab41785c146b0387a7be7fe8cc86d126
Adam Tiller <a.j.tiller@gmail.com>
2020-04-30 10:55:07 -0700

    Add seeding function with param for number of records to seed.

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+const seed = require('./seeder.js');
 
 mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
   console.log('Connection to database successful!');
   const reviewSchema = new mongoose.Schema({
     gameId: Number,
-    date: {type: Date, default: Date.now},
+    date: { type: Date, default: Date.now },
     overall: Number,
     title: String,
     review: String,
@@ -29,44 +30,20 @@ db.once('open', function() {
     recommendBGS: Number,
     meta: {
       helpful: Number,
-      unhelpful: Number
-    }
+      unhelpful: Number,
+    },
   });
 
   const Review = mongoose.model('Review', reviewSchema);
 
-  // const reviewOne = new Review({
-  //   gameId: 1,
-  //   overall: 4,
-  //   title: 'The Greatest Game',
-  //   review: 'This game is the greatest!',
-  //   recommend: true,
-  //   nickname: 'Reviewer Two',
-  //   location: 'Citytown, USA',
-  //   email: 'name@domain.com',
-  //   buyForSelf: true,
-  //   ageBracket: 2,
-  //   gender: 2,
-  //   graphics: 4,
-  //   gameplay: 5,
-  //   appeal: 3,
-  //   ownershipBracket: 2,
-  //   purchaseOnline: true,
-  //   readReviews: false,
-  //   recommendBGS: 1,
-  //   meta: {
-  //     helpful: 25,
-  //     unhelpful: 11
-  //   }
-  // });
-
-  // reviewOne.save((err, results) => {
+  // UNCOMMENT BELOW TO DROP COLLECTION AND RESEED DATA
+  //
+  // mongoose.connection.db.dropCollection('reviews', (err) => {
   //   if (err) {
-  //     console.log(err)
+  //     console.log(err);
   //     return;
   //   }
-
-  //   console.log(results);
+  //   seed.seed(Review, 100);
   // });
 });
 
