622093db2778975c4c71c2812c3998f422ee595e
Adam Tiller <a.j.tiller@gmail.com>
2020-05-01 09:58:07 -0700

    Add handling for gameId via query string.

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = (props) => (
-  <div>
-    <h4>React is online!</h4>
-    And Webpack is watching!
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      currentGame: 0,
+      reviews: []
+    };
+  }
+
+  componentDidMount() {
+    const urlParams = new URLSearchParams(window.location.search);
+    this.setState({
+      currentGame: urlParams.get('gameId'),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>React is online!</h4>
+        And Webpack is watching!<br />
+        Now viewing the data for game {this.state.currentGame}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
