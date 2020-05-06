fe02281f25e402779214be574d43f87b814bb4f6
Adam Tiller <a.j.tiller@gmail.com>
2020-05-05 17:22:02 -0700

    Switch ReviewListItem to grid and styled-components.

--- client/components/ReviewListItem.jsx
+++ client/components/ReviewListItem.jsx
@@ -1,35 +1,74 @@
 import React from 'react';
 import _ from 'lodash';
+import styled from 'styled-components';
+
+const Container = styled.div`
+display: grid;
+grid-template-columns: 60px 7fr 3fr;
+grid-template-rows: 60px 60px 60px 60px 60px;
+grid-template-areas:
+"avatar header    badge"
+"   .   review    ratings"
+"   .   owned     ratings"
+"   .   recommend ratings"
+"   .   helpful   helpful";
+`;
+
+const Avatar = styled.div`
+grid-area: avatar;
+background: #b3e2cd;
+`;
+
+const Header = styled.div`
+grid-area: header;
+background: #fdcdac;
+`;
+
+const Badge = styled.div`
+grid-area: badge;
+background: #cbd5e8;
+`;
+
+const Review = styled.div`
+grid-area: review;
+background: #f4cae4;
+`;
+
+const Owned = styled.div`
+grid-area: owned;
+background: #e6f5c9;
+`;
+
+const Recommend = styled.div`
+grid-area: recommend;
+background: #f1e2cc;
+`;
+
+const Ratings = styled.div`
+grid-area: ratings;
+background: #cccccc;
+`;
+
+const Helpful = styled.div`
+grid-area: helpful;
+background: #fff2ae;
+`;
+
 
 const ReviewListItem = (props) => {
-  const {
-    date,
-    title,
-    review,
-    nickname,
-    location,
-    overall,
-    graphics,
-    gameplay,
-    appeal
-  } = props.review;
+  const { review } = props;
 
   return (
-    <div>
-      <h4>{_.startCase(title)}</h4>
-      <span>{nickname}</span>
-      {` (${location})`}
-      <p>
-        {review}
-      </p>
-      Overall: {overall}
-      <br />
-      Graphics: {graphics}
-      <br />
-      Gameplay: {gameplay}
-      <br />
-      Lasting Appeal: {appeal}
-    </div>
+    <Container>
+      <Avatar />
+      <Header />
+      <Badge />
+      <Review>{review.review}</Review>
+      <Owned />
+      <Recommend />
+      <Ratings />
+      <Helpful />
+    </Container>
   );
 };
 
